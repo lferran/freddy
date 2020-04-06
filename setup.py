830d10d8cd27a7ffe8005aee30692bb77e8bf852
Ferran Llamas <llamas.arroniz@gmail.com>
2020-04-06 09:26:05 +0200

    fix

--- setup.py
+++ setup.py
@@ -12,8 +12,6 @@ setup(
     version=VERSION,
     long_description=open(os.path.join(_dir, "README.md")).read(),
     classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules",
